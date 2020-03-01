d680a3e1ca1bcdd585ed70069cedc3bb2b3983d4
Marcel <marshby@gmail.com>
2020-03-01 19:25:26 +0100

    First

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reply2018Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
